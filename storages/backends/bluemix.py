6439e7e913a0aa844d0c0daf3bb1ae698da2cc2d
Sanjay Joshi <joshisa@us.ibm.com>
2015-10-01 16:53:14 -0400

    Update bluemix.py

--- storages/backends/bluemix.py
+++ storages/backends/bluemix.py
@@ -81,6 +81,3 @@ class BluemixStorage(Storage):
 
     def url(self, name):
         return "{}/{}/{}".format(self.connection.properties['url'],self.container_name, name)
-    
-    def path(self, name):
-        return None
